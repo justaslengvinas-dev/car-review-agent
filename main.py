0800128d2840a3869e5aafcf11f609c0c929933c
justaslengvinas-dev <justas.lengvinas@gmail.com>
2026-05-07 12:45:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import feedparser
 import yaml
 
-# Užkrauname šaltinius iš YAML failo
 with open("sources.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 sources = config["sources"]
 
-# Einame per visus RSS šaltinius
 for source in sources:
     print(f"\nTikrinamas šaltinis: {source['name']}")
+    print(f"URL: {source['url']}")
 
     feed = feedparser.parse(source["url"])
 
-    # Paimame pirmus 5 straipsnius
+    print("Rasta įrašų:", len(feed.entries))
+
+    if len(feed.entries) == 0:
+        print("Šis RSS šaltinis šiuo metu negrąžina įrašų.")
+        continue
+
     for entry in feed.entries[:5]:
         print("-" * 50)
         print("Pavadinimas:", entry.title)
