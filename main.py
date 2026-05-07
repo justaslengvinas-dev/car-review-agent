c4f0883cd65c7e699482a583cb430169af6f512b
justaslengvinas-dev <justas.lengvinas@gmail.com>
2026-05-07 12:48:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import feedparser
 import yaml
+import requests
 
 with open("sources.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 sources = config["sources"]
 
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
 for source in sources:
     print(f"\nTikrinamas šaltinis: {source['name']}")
     print(f"URL: {source['url']}")
 
-    feed = feedparser.parse(source["url"])
+    response = requests.get(source["url"], headers=headers, timeout=20)
+
+    print("HTTP statusas:", response.status_code)
+    print("Atsisiųsta simbolių:", len(response.text))
+
+    feed = feedparser.parse(response.text)
 
     print("Rasta įrašų:", len(feed.entries))
-
-    if len(feed.entries) == 0:
-        print("Šis RSS šaltinis šiuo metu negrąžina įrašų.")
-        continue
 
     for entry in feed.entries[:5]:
         print("-" * 50)
